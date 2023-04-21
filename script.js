a10d5a5dbaf5ea83ce1387ceb0c5c73fd9a9c250
Juliana Rosa <90176648+Joullie@users.noreply.github.com>
2023-04-21 01:04:01 -0300

    Correção de geolocalização.
    Correção para solicitar permissão ao local no mobile. Talvez não funcione no Safari.

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-const apiKey = 'd96c31f827c34768313a36e97e723dd8'
 const apiUrl = 'https://api.openweathermap.org/data/2.5/weather'
 
 // Verifica se as informações de localização estão no localStorage.
@@ -22,31 +21,39 @@ if (storedLocation) {
     .catch(error => console.error(error))
 } else {
   // Solicita perm de localização.
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude, longitude } = position.coords
-      localStorage.setItem('location', JSON.stringify({ latitude, longitude }))
-      const url = `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
+  if ('geolocation' in navigator) {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        const { latitude, longitude } = position.coords
+        localStorage.setItem(
+          'location',
+          JSON.stringify({ latitude, longitude })
+        )
+        const url = `${apiUrl}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`
 
-      fetch(url)
-        .then(response => response.json())
-        .then(data => {
-          const { name, sys, main, weather } = data
-          document.getElementById('city').textContent = name
-          document.getElementById('country').textContent = sys.country
-          document.getElementById('temp').textContent = `${Math.round(
-            main.temp
-          )}°C`
-          document.getElementById('desc').textContent = weather[0].description
-          document.getElementById(
-            'icon'
-          ).src = `https://openweathermap.org/img/w/${weather[0].icon}.png`
-        })
-        .catch(error => console.error(error))
-    },
-    error => {
-      console.error(error)
-      alert('Impossível obter localização.')
-    }
-  )
+        fetch(url)
+          .then(response => response.json())
+          .then(data => {
+            const { name, sys, main, weather } = data
+            document.getElementById('city').textContent = name
+            document.getElementById('country').textContent = sys.country
+            document.getElementById('temp').textContent = `${Math.round(
+              main.temp
+            )}°C`
+            document.getElementById('desc').textContent = weather[0].description
+            document.getElementById(
+              'icon'
+            ).src = `https://openweathermap.org/img/w/${weather[0].icon}.png`
+          })
+          .catch(error => console.error(error))
+      },
+      error => {
+        console.error(error)
+        alert('Impossível obter localização.')
+      }
+    )
+  } else {
+    console.log('Geolocalização não está disponível.')
+  }
 }
+
